e2972af32d5bd7c8f9c37c1c95dc890f4f87da14
Jean-Baptiste CAMBILLARD <jeanbaptiste.cambillard@gmail.com>
2022-07-29 13:42:48 +0200

    band + nav css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import "./App.scss";
 import React from "react";
+import FacebookIcon from "./assets/fb.png";
+import InstaIcon from "./assets/inst.png";
+import TwitterIcon from "./assets/tw.png";
+import LightMode from "./assets/Vector.png";
 
 function App() {
   return (
     <div className="App">
-      <div className="myapp">Hello</div>
+      {/* Start Left Band */}
+      <div className="left__band">
+        <h1 className="left__band-logo">Xae</h1>
+        <div className="left__band-icons">
+          <div className="facebook">
+            <img
+              src={FacebookIcon}
+              alt="FacebookIcon"
+              className="facebook__icon"
+            />
+          </div>
+          <div className="instagram">
+            <img src={InstaIcon} alt="InstaIcon" className="instagram__icon" />
+          </div>
+          <div className="twitter">
+            <img
+              src={TwitterIcon}
+              alt="twitterIcon"
+              className="twitter__icon"
+            />
+          </div>
+        </div>
+      </div>
+      {/* End Left Band */}
+      {/* Start Nav */}
+      <div className="nav">
+        <h3 className="nav__options">Home</h3>
+        <h3 className="nav__options">About</h3>
+        <h3 className="nav__options">Gallery</h3>
+        <h3 className="nav__options">Contact</h3>
+        <img src={LightMode} alt="" className="nav__mode" />
+      </div>
+      {/* End Nav */}
     </div>
   );
 }
--- src/components/headband/Headband.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Headband.scss";
-
-function Headband() {
-  return <div>Headband</div>;
-}
-
-export default Headband;
